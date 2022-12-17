7be7785485dca904770452242381bcaed92d9510
ositrova <110326755+ositrova@users.noreply.github.com>
2022-12-17 16:39:09 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -18,18 +18,14 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 // console.log(galleryItems);
-
 const containerGallary = document.querySelector('.gallery');
 
 const imgItems = galleryItems.map(({ preview, original, description }) =>
+
 `<div class="gallery__item">
-<a class="gallery__link" href="${original}"
-<img
-class="gallery__image"
-src="${preview}"
-data-source="${original}"
-alt="${description}"
-/>
+<a class="gallery__item" href="${original}">
+<img class="gallery__image" src="${preview}" 
+data-source="${original}" alt="${description}"/>
 </a>
 </div>`).join('');
 
